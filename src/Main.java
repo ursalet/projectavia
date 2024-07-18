d1e4519c7b10d896102a3cde870180341cd0aec5
Elena Zhikhareva <ursalet@gmail.com>
2024-07-18 10:02:39 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 14350;
+        int oneBonusMile = 20;
+
+        int bonus = price / oneBonusMile;
+
+        System.out.println(bonus);
+
+    }
+}
