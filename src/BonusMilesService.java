8deff31a056d20cb4dc170a948f0f2ce121d08e9
Elena Zhikhareva <ursalet@gmail.com>
2024-07-24 21:27:44 +0400

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,6 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int oneBonusMile = 20;
+        return price / oneBonusMile;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int price = 14350;
-        int oneBonusMile = 20;
-
-        int bonus = price / oneBonusMile;
-
-        System.out.println(bonus);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
